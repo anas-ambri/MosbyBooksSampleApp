8136ad378708b04f76383791ed776e5f5ea00067
Anas Ambri <anas.ambri@gmail.com>
2016-09-12 21:57:31 -0400

    Add one instrumentation test to test transition of data between list and details

--- app/src/androidTest/java/com/verybadalloc/books/ApplicationTest.java
+++ app/src/androidTest/java/com/verybadalloc/books/ApplicationTest.java
@@ -1,13 +1,33 @@
 package com.verybadalloc.books;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+import com.verybadalloc.books.views.BooksListActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ApplicationTest {
+
+    @Rule
+    public ActivityTestRule<BooksListActivity> activityTestRule = new ActivityTestRule<>(BooksListActivity.class);
+
+    @Test
+    public void listBooksClickOpenDetails() {
+        onView(withId(R.id.recyclerView)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.book_author)).check(matches(withText("Paula Hawkins")));
     }
 }
